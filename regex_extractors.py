d5f5b7e32a14fe6d09c4f304156edb51896251ed
TRolande <r.tumugane@alustudent.com>
2025-05-22 12:49:14 +0200

    this will be the file to test my entries

--- regex_extractors.py
+++ regex_extractors.py
@@ -1,21 +1,25 @@
 import re
+# This will help to import the regular expressions module thati am going to use.
 
 def extract_phone_numbers(text):
+    # This Function will help to extract phone numbers from the ones entered.
     pattern = r'(\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4})'
+     # This Pattern will match formats like (123) 456-7890, 123-456-7890, 123.456.7890, or 123 456 7890
     return re.findall(pattern, text)
 
+# Function will extract email addresses from the entries
 def extract_emails(text):
     pattern = r'[\w\.-]+@[\w\.-]+\.\w+'
     return re.findall(pattern, text)
-
+#This function will extract the currency following the condition which is in the regex expression
 def extract_currency(text):
     pattern = r'\$\d{1,3}(?:,\d{3})*(?:\.\d{2})?'
     return re.findall(pattern, text)
-
+#This function will be for the html tags and their conditions which are written in the expression.
 def extract_html_tags(text):
     pattern = r'<[^>]+>'
     return re.findall(pattern, text)
-
+#This will be the fuction for extracting url 
 def extract_urls(text):
     pattern = r'https?://[^\s<>"\']+|www\.[^\s<>"\']+'
     return re.findall(pattern, text)
--- test_case.py
+++ test_case.py
@@ -1,11 +1,20 @@
 from regex_extractors import *
 
+#This will open the file data_samples.txt in read mode and i will use UTF-8 for encoding
+#and this file will contain all the sample text for extraction.
+
 with open('data_samples.txt', 'r', encoding='utf-8') as file:
+    #This will read this file whole content and put it into string
     content = file.read()
+    
 
-
+#This will call the function for phone numbers and print the result.
 print("Phone Numbers:", extract_phone_numbers(content))
+#This will call the function of email and print the results.
 print("Emails:", extract_emails(content))
+#This will call the function for currency and print the results.
 print("Currency:", extract_currency(content))
+#This will call the function for html tags and print the results.
 print("HTML Tags:", extract_html_tags(content))
+#This will call the function for urls and print the results.
 print("URLs:", extract_urls(content))
